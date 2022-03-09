49731097e528c14a924e1bc154c1471221302fb3
Andrewowalla <andrewowalla@gmail.com>
2022-03-09 15:27:39 +0300

    Add urls for register and login views

--- photoapp/urls.py
+++ photoapp/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from photoapp.views import register, login_view
 
 urlpatterns=[
     path('', views.homepage, name='homepage'),
+    path('register/', register, name = "register"),
+    path('login/', login_view, name = "login" )
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
