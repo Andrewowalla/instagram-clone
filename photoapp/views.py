5d46fedb20406cd065c4357cbfe9cd0c6a82fc72
Andrewowalla <andrewowalla@gmail.com>
2022-03-09 15:24:20 +0300

    Add a view Function for register

--- photoapp/views.py
+++ photoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.shortcuts import render, redirect
+from photoapp.forms import UserRegistrationForm, UserLoginForms
 
 # Create your views here.
 
@@ -7,3 +9,17 @@ def homepage(request):
     images = Image.objects.all()
     return render(request, 'home.html', {"images":images})
 
+def register(request):
+    context = {}
+    if request.POST:
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        context['register_form'] = form
+
+    else:
+        form = UserRegistrationForm()
+        context['register_form'] = form
+
+    return render(request, "registration/register.html", context)
