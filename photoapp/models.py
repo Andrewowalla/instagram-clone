8fa2145005bad429d0364675fe8387fc5ee0b0b5
Andrewowalla <andrewowalla@gmail.com>
2022-03-09 10:36:23 +0300

    Create Profile model

--- photoapp/models.py
+++ photoapp/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Profile(models.Model):
+    profilepic = models.ImageField(upload_to = 'image')
+    bio = models.CharField(max_length=100)
+    username = models.CharField(max_length=20)
+
 class Likes(models.Model):
     likes = models.IntegerField(default=0)
 
@@ -9,6 +14,6 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length=25)
     caption = models.CharField(max_length=150)
-    profile = models.ForeignKey( on_delete=models.CASCADE, default=None)
+    profile = models.ForeignKey(Profile,  on_delete=models.CASCADE, default=None)
     likes = models.ForeignKey(Likes, on_delete=models.CASCADE, default=0)
     comment = models.CharField(max_length=150)
