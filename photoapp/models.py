19f2b323e55d0fdf6ec0871841642d8cf1a12d4f
Andrewowalla <andrewowalla@gmail.com>
2022-03-09 15:11:01 +0300

    Add model class MyUser

--- photoapp/models.py
+++ photoapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
 
@@ -26,3 +28,30 @@ class Image(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+class MyUser(AbstractBaseUser):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length= 50)
+    username = models.CharField(max_length = 50, unique = True)
+    email = models.EmailField(max_length=60, unique=True)
+    phone_number = models.CharField(max_length = 50)
+    date_joined = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    # objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
