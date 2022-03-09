064daa0711d32916bc7b00328fc805cca4c5101f
Andrewowalla <andrewowalla@gmail.com>
2022-03-09 10:31:26 +0300

    Create Likes model

--- photoapp/models.py
+++ photoapp/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Likes(models.Model):
+    likes = models.IntegerField(default=0)
+
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length=25)
     caption = models.CharField(max_length=150)
     profile = models.ForeignKey( on_delete=models.CASCADE, default=None)
-    likes = models.ForeignKey( on_delete=CASCADE, default=0)
+    likes = models.ForeignKey(Likes, on_delete=models.CASCADE, default=0)
     comment = models.CharField(max_length=150)
