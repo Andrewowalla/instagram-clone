b565f11e583c8e5b217883fd20611428ab22b9a2
Andrewowalla <andrewowalla@gmail.com>
2022-03-09 11:16:06 +0300

    Add setting that will display our uploaded images

--- photoapp/views.py
+++ photoapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
-# def homepage(request):
+def homepage(request):
+    images = Image.objects.all()
+    return render(request, 'home.html', {"images":images})
