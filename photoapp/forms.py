33dcf73e026be499bdf30a257756702dd15499eb
Andrewowalla <andrewowalla@gmail.com>
2022-03-10 15:57:00 +0300

    Create a UpdateProfileForm for updating users profile

--- photoapp/forms.py
+++ photoapp/forms.py
@@ -28,3 +28,9 @@ class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['date_created', 'likes', 'user']
+        
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('username', 'bio', 'profile_photo')
+        exclude = ['user']
