b7187ed7c73114a5b6b025bec7b987a8a3e60730
Andrewowalla <andrewowalla@gmail.com>
2022-03-11 10:52:27 +0300

    Create view functions for viewing profile

--- photoapp/views.py
+++ photoapp/views.py
@@ -1,11 +1,12 @@
 from multiprocessing import context
 from django.core.exceptions import ObjectDoesNotExist
 from django.http.response import Http404
+from django.urls import reverse
 from django.shortcuts import render
 from .models import *
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from photoapp.forms import UserRegistrationForm, NewImageForm, LoginForm
+from photoapp.forms import UserRegistrationForm, NewImageForm, LoginForm, UpdateProfileForm
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
@@ -68,6 +69,34 @@ def image(request, id):
         'comments': comments
     }
     return render(request, 'image.html', context)
+
+def view_profile(request, id):
+    try:
+        profile = Profile.objects.get(id = id)
+        context = {
+            'profile': profile,                       
+        }
+        return render(request, 'profile.html', context)
+    except:
+        messages.warning(request, 'Sorry, but it seems the profile is not set up')
+        return redirect('home')
+  
+
+def update_profile(request):
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        context = {
+            'form': form
+        }
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('view_profile', kwargs={"id": request.user.profile.id }))
+        else:
+            messages.warning(request, 'There was a problem updating your profile')
+            return redirect('update_profile')
+    else:
+        form = UpdateProfileForm(instance=request.user.profile)
+        return render(request, 'update_profile.html', {"form":form})
 
 def register(request):
     context = {}
